286e2390c1e1ddbdfa7dca162e5341290a146d41
havardlj <havard@skogen.io>
2020-07-02 00:01:14 +0200

    done

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,33 @@ window.addEventListener('DOMContentLoaded', objectFitImages);
 
 import './utils.exec';
 import 'lazysizes';
-/* import Swiper from 'swiper'; */
+import Swiper from 'swiper';
 /* import 'accordion.exec'; */
 /* import ScrollOut from 'scroll-out'; */
+
+var $swiper = new Swiper ('.swiper-container', {
+  slidesPerView: 'auto'
+  /* Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  }, */
+})
+
 
 /*var headroom  = new Headroom($('header')[0]);
 headroom.init();*/
